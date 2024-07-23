bb74b6a7c5526e1f3034ea02ea0fcf481714fb68
thevorgx <kh.elallali@gmail.com>
2024-07-23 01:07:20 +0100

    task 0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+export function taskFirst() {
+    var task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    var combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
