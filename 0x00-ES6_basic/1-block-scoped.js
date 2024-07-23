7dd16249f5222d927f7875ce5ef44309b8f3a8fe
thevorgx <kh.elallali@gmail.com>
2024-07-23 02:01:00 +0100

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
