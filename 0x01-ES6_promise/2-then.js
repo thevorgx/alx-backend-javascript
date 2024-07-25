9cafd147d651024018fcb327b32da4aaf4c8fb7d
thevorgx <kh.elallali@gmail.com>
2024-07-25 02:06:14 +0100

    update number 364S63768379309469435604579

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
-export default function handleResponseFromAPI(promise) {
-  const myprom = promise
-    .then(() => ({ status: 200, body: 'Success' }))
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
-  return myprom;
 }
+export default handleResponseFromAPI;
