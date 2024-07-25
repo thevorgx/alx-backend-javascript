e6ca171aea688a35a8d11c9768eb014c897dcc41
thevorgx <kh.elallali@gmail.com>
2024-07-26 00:54:12 +0100

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const cr1 = new ClassRoom(19);
+  const cr2 = new ClassRoom(20);
+  const cr3 = new ClassRoom(34);
+  const arrObj = [cr1, cr2, cr3];
+  return (arrObj);
+}
