d9d5372fc122edff79160bebf2e6cd71052fe5d9
thevorgx <kh.elallali@gmail.com>
2024-07-25 03:33:32 +0100

    task 4

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,6 @@
+function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    resolve({ firstName: `${firstName}`, lastName: `${lastName}` });
+  });
+}
+export default signUpUser;
