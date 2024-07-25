fb37ac283f99f5d6c0793d14657caafb163ae126
thevorgx <kh.elallali@gmail.com>
2024-07-25 01:56:52 +0100

    update

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,7 @@
 function handleResponseFromAPI(promise) {
-  const myprom = promise
+  return promise
     .then(() => ({ status: 200, body: 'Success' }))
     .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
-  return myprom;
 }
 export default handleResponseFromAPI;
