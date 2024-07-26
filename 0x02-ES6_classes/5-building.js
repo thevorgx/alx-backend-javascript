101a4259c06c007bcb6a00abac7803b12ce849f1
thevorgx <kh.elallali@gmail.com>
2024-07-26 03:46:58 +0100

    task5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+export default class Building {
+  constructor(sqft) {
+    if (new.target === Building) {
+      this._sqft = sqft;
+    } else {
+      this.evacuationWarningMessage();
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    if (this._sqft === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+}
