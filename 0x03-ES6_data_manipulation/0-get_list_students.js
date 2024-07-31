faecc1966527358e8b56dbfa5acbd8a272d47da2
thevorgx <kh.elallali@gmail.com>
2024-07-31 01:57:45 +0100

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+export default function getListStudentIds() {
+  const p1 = {
+    id: 1,
+    name: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const p2 = {
+    id: 2,
+    name: 'James',
+    location: 'Columbia',
+  };
+  const p3 = {
+    id: 5,
+    name: 'Serena',
+    location: 'San Francisco',
+  };
+  const ArrObj = [p1, p2, p3];
+  return (ArrObj);
+}
