8c74ad000b4be62c101cf751ee785ced3215ca9a
thevorgx <kh.elallali@gmail.com>
2024-07-31 03:54:15 +0100

    task 7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function hasValuesFromArray(set, array) {
+  for (const value of array) {
+    if (set.has(value)) {
+      return (true);
+    }
+  }
+  return (false);
+}
