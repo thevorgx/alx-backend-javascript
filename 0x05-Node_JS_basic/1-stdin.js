94add06c4b10374ab489bcbd694120baa19dce0a
thevorgx <kh.elallali@gmail.com>
2024-09-18 22:46:34 +0100

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-    const name = input.toString().trim();
-    process.stdout.write(`Your name is: ${name}`);
-    process.stdin.pause();
+process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit();
 });
 
 process.on('exit', () => {
-    console.log('This important software is now closing');
+    process.stdout.write('This important software is now closing\n');
 });
