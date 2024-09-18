33b19b696af0d6facfedf40616ec473543e3325c
thevorgx <kh.elallali@gmail.com>
2024-09-19 00:25:35 +0100

    /ff

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,63 +1,64 @@
 const express = require('express');
-const fs = require('fs');
+
+const { readFile } = require('fs');
 
 const app = express();
-const port = 1245;
 const hostname = '127.0.0.1';
+const port = 1245;
 
-const countStudents = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-      return;
-    }
-
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const students = lines.slice(1);
-
-    const fields = {};
-
-    students.forEach((student) => {
-      const [firstname, , , field] = student.split(',');
-      if (!fields[field]) {
-        fields[field] = [];
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
       }
-      fields[field].push(firstname);
     });
+  });
+}
 
-    let output = `Number of students: ${students.length}\n`;
-    for (const [field, names] of Object.entries(fields)) {
-      output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
-    }
-
-    resolve(output);
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2].toString()).then((output) => {
+    res.send(['This is the list of our students', output].join('\n'));
+  }).catch(() => {
+    res.send('This is the list of our students\nCannot load the database');
   });
 });
 
-app.get('/', (_, res) => {
-  res.send('Hello Holberton School!');
-});
-
-app.get('/students', (req, res) => {
-  const databasePath = process.argv[2];
-
-  res.write('This is the list of our students');
-
-  if (!databasePath) {
-    res.end('Error: No database file path provided');
-  } else {
-    countStudents(databasePath)
-      .then((output) => {
-        res.end(output);
-      })
-      .catch(() => {
-        res.end('Cannot load the database');
-      });
-  }
-});
-
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
