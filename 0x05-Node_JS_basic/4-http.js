8ea9f859fceb73cf2c2731ade394ef0653f2760f
thevorgx <kh.elallali@gmail.com>
2024-09-18 23:38:24 +0100

    update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,7 +5,7 @@ const port = 1245;
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
