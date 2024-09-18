23752d22fb8f0dc2bf7cb073b5eb4bcb9b6be8d1
thevorgx <kh.elallali@gmail.com>
2024-09-18 23:22:17 +0100

    eslint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
     if (lines.length === 0) throw new Error('Cannot load the database');
 
