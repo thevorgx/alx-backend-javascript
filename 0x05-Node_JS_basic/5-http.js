33c398d51f5c04a10269a17e94c94b2656684e44
thevorgx <kh.elallali@gmail.com>
2024-09-19 02:03:51 +0100

    update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,39 +1,53 @@
 const http = require('http');
-
-const url = require('url');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
 const hostname = '127.0.0.1';
 const port = 1245;
-const app = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
+async function countStudents(path) {
+  try {
+    const studentData = await fs.readFile(path, 'utf8');
+    let res = '';
+    const students = studentData
+      .split('\n')
+      .filter((student) => student.length > 0)
+      .map((student) => student.split(','));
 
-  if (parsedUrl.pathname === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!\n');
-  } else if (parsedUrl.pathname === '/students') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('This is the list of our students\n');
+    students.shift();
+    res += `Number of students: ${students.length}`;
+    const filedOfStudy = {};
+    students.forEach((student) => {
+      if (!filedOfStudy[student[3]]) filedOfStudy[student[3]] = [];
+      filedOfStudy[student[3]].push(student[0]);
+    });
 
-    const databasePath = process.argv[2];
+    Object.keys(filedOfStudy).forEach((key) => {
+      res += `\nNumber of students in ${key}: ${
+        filedOfStudy[key].length
+      }. List: ${filedOfStudy[key].join(', ')}`;
+    });
+    return res;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
 
-    if (!databasePath) {
-      res.end('Error: No database file path provided\n');
-    } else {
-      countStudents(databasePath)
-        .then(() => {
-          res.end();
-        })
-        .catch((err) => {
-          res.end(`${err.message}\n`);
-        });
-    }
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not Found\n');
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  const msg = 'This is the list of our students';
+  switch (req.url) {
+    case '/':
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      try {
+        res.end(`${msg}\n${await countStudents(process.argv[2])}`);
+      } catch (error) {
+        res.statusCode = 404;
+        res.end(`${msg}\n${error.message}`);
+      }
+      break;
+    default:
+      break;
   }
 });
 
