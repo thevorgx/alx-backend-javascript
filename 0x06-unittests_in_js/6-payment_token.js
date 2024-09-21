1e5ae2c772fdc79676b7b888e3a7f925f0d90c01
thevorgx <kh.elallali@gmail.com>
2024-09-21 02:10:28 +0100

    task 6

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+const getPaymentTokenFromAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ data: 'Successful response from the API' });
+    }
+  });
+};
+module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should return a resolved promise with the correct data', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.have.property('data', 'Successful response from the API');
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
