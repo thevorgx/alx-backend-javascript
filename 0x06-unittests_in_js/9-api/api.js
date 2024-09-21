35c6f88b7cab0f5c6365677ac83177cb0b758f3f
thevorgx <kh.elallali@gmail.com>
2024-09-21 02:29:28 +0100

    task 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,1 +1,22 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  const cartId = req.params.id;
+  res.send(`Payment methods for cart ${cartId}`);
+});
+
+app.get('/cart/:id', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+module.exports = app;
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,47 @@
-#!/usr/bin/node
+const chai = require('chai');
+const request = require('request');
+const expect = chai.expect;
+
+describe('Index page', function() {
+  const url = 'http://localhost:7865';
+
+  it('should return status code 200', function(done) {
+    request(url, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct result', function(done) {
+    request(url, function(error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should not have an error', function(done) {
+    request(url, function(error, response, body) {
+      expect(error).to.be.null;
+      done();
+    });
+  });
+});
+
+describe('Cart page', function() {
+  const baseUrl = 'http://localhost:7865/cart/';
+
+  it('should return status code 200 for a valid cart id', function(done) {
+    request(baseUrl + '12', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return 404 for a non-numeric cart id', function(done) {
+    request(baseUrl + 'hello', function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
