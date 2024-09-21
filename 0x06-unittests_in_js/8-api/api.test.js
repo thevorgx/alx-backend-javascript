c4f18e600cb141f489623991ff1ef08b9581c973
thevorgx <kh.elallali@gmail.com>
2024-09-21 02:22:12 +0100

    task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+const chai = require('chai');
+const request = require('request');
+const expect = chai.expect;
+
+describe('Index page', function() {
+  const url = 'http://localhost:7865';
+
+  it('should return status code 200', function(done) {
+    request(url, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct result', function(done) {
+    request(url, function(error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should not have an error', function(done) {
+    request(url, function(error, response, body) {
+      expect(error).to.be.null;
+      done();
+    });
+  });
+});
